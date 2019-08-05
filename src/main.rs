147eda7eab27d6b04cf0616a1bab05c65cbe6b26
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2019-08-05 18:49:36 +0200

    use clap for parsing args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-extern crate rand;
-extern crate sort;
+use std::process;
 
 use rand::prelude::*;
-use std::env::args;
+use clap::{Arg,App};
 
 mod player;
 use player::play;
@@ -20,48 +19,61 @@ const WIDTH: usize = 800;
 const HEIGHT: usize = 600;
 const MARGIN: usize = 2;
 
-fn parse_args(args: Vec<String>) -> Result<(fn(&mut List<u32>), Vec<u32>), &'static str> {
-    // Select sorting algorithm
-    let sort_fn = match args.get(1) {
-        Some(arg) => match arg.as_str() {
-            "bubblesort" => bubblesort,
-            "selectionsort" => selectionsort,
-            "quicksort" => quicksort,
-            "quicksort2" => quicksort2,
-            _ => return Err("Unknown sorting algorithm in first parameter"),
-        },
-        None => return Err("Missing first parameter: Sorting algorithm"),
-    };
-    // Generate data
-    if let Some(arg) = args.get(2) {
-        if let Ok(datalength) = arg.parse::<usize>() {
-            let mut data = Vec::with_capacity(datalength);
-            for _ in 0..datalength {
-                data.push(random::<u32>());
-            }
-            Ok((sort_fn, data))
-        } else {
-            Err("Invalid second parameter: Cannot parse data size as number")
-        }
-    } else {
-        Err("Missing second parameter: Data size")
+macro_rules! error {
+    ($($arg:tt)*) => ({
+        eprintln!($($arg)*);
+        process::exit(1);
+    })
+}
+
+fn get_sort_fn(name: &str) -> Option<fn(&mut List<u32>)> {
+    match name {
+        "bubblesort" => Some(bubblesort),
+        "selectionsort" => Some(selectionsort),
+        "quicksort" => Some(quicksort),
+        "quicksort2" => Some(quicksort2),
+        _ => None,
     }
 }
 
-fn main() {
-    // Data
-    let sort_fn;
-    let data;
-    // Parse args
-    match parse_args(args().collect()) {
-        Ok((func, dat)) => {
-            sort_fn = func;
-            data = dat;
-        }
-        Err(msg) => {
-            panic!(format!("Error parsing arguments -> {}", msg));
-        }
+fn parse_args() -> (fn(&mut List<u32>), Vec<u32>) {
+    let matches = App::new("Sort GUI")
+        .version("0.1.0")
+        .author("Jon Ayerdi")
+        .about("Visualize sorting algorithms")
+        .arg(Arg::with_name("sort")
+            .short("s")
+            .long("sort")
+            .value_name("ALGORITHM")
+            .help("Sorting algorithm to use")
+            .required(true))
+        .arg(Arg::with_name("rand")
+            .short("r")
+            .long("rand")
+            .value_name("COUNT")
+            .help("Count of random elements to sort")
+            .required(true))
+        .get_matches();
+    let sort_fn_str = matches.value_of("sort").unwrap();
+    let sort_fn = match get_sort_fn(sort_fn_str) {
+        Some(fptr) => fptr,
+        None => error!("error: Sorting function \"{}\" not found", sort_fn_str),
+    };
+    let rand_count_str = matches.value_of("rand").unwrap();
+    let rand_count = match rand_count_str.parse::<usize>() {
+        Ok(n) => n,
+        Err(_) => error!("error: Cannot parse \"{}\" as unsigned number", rand_count_str),
+    };
+    let mut data = Vec::with_capacity(rand_count);
+    for _ in 0..rand_count {
+        data.push(random::<u32>());
     }
+    (sort_fn, data)
+}
+
+fn main() {
+    // Parse args
+    let (sort_fn, data) = parse_args();
     // Init
     let visualization = ListVisualization::autogenerate(&data, WIDTH, HEIGHT, MARGIN);
     let window = ListVisualizationWindow::new(visualization);
